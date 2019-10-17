ecbc8ed6e4ab37dd442c8c5a1868d2abe2b57c92
Will Diep <widiep@gmail.com>
2019-10-17 09:04:57 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
-# Your Code Here
+def map(arr)
+	result = []
+	i = 0
+	while i < arr.length do
+		result.push(yield(arr[i]))
+		i = i + 1
+	end
+	return result
+end
+
+# p (map([1, 2, 3, -9]) do |n|
+# 	n * -1
+# end)
+
+# p (map(["paul", "gurney", "vladimir", "jessica", "chani"]) { |n| n })
+
+# p (map([1, 2, 3, -9]) do |n|
+# 	n * n
+# end)
+
+
+def reduce(array, sp=nil)
+  if sp
+    total = sp
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  while i < array.length
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end
